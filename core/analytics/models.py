04c892db5d9af60c67e42791575648795067924c
karianjahi <joseph@spiced-academy.com>
2026-05-12 16:15:02 +0200

    Add campaign date

--- core/analytics/models.py
+++ core/analytics/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class CampaignData(models.Model):
+    date = models.DateField()
+    campaign_name = models.CharField(max_length=255)
+    channel = models.CharField(max_length=100)
+    impressions = models.IntegerField()
+    clicks = models.IntegerField()
+    conversions = models.IntegerField()
+    cost = models.DecimalField(max_digits=12, decimal_places=2)
+    revenue = models.DecimalField(max_digits=12, decimal_places=2)
+    
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.campaign_name} - {self.date}"
+    
