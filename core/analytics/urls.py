cbaf4180c3c0956ba9c54d1e92bf2874b04bd24d
karianjahi <joseph@spiced-academy.com>
2026-05-12 21:45:53 +0200

    Define urls

--- core/analytics/urls.py
+++ core/analytics/urls.py
@@ -3,5 +3,5 @@ from .import views
 
 urlpatterns = [
     path("", views.dashboard,name="dashboard"),
-    path("upload/", views.upload_csv, name="upload_csv")
+    path("upload/", views.upload_csv, name="upload_csv"),
 ]
