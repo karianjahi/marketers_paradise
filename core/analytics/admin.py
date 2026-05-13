e004fd69fee879e4837d16903bfb437e5a3c6733
karianjahi <joseph@spiced-academy.com>
2026-05-13 13:32:40 +0200

    Modify admin.py to create flexibility in terms of what columns to show in the admin panel and a filter functionality to filter list by (a) certain column(s)

--- core/analytics/admin.py
+++ core/analytics/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
 from .models import CampaignData
 
-admin.site.register(CampaignData)
+
+@admin.register(CampaignData)
+class CampaignDataAdmin(admin.ModelAdmin):
+    list_display = (
+        "campaign_name",
+        "channel",
+        "date",
+        # "impressions",
+        # "clicks",
+        # "conversions",
+        # "cost",
+        # "revenue",
+        # "uploaded_at",
+    )
+    list_filter = (
+        "channel",
+        # "date",
+    )
+    search_fields = (
+        "campaign_name",
+        # "channel",
+    )
