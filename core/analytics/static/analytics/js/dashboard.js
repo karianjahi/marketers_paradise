989f511b4c9f14c187ec63d69b2ea15879185ed1
karianjahi <joseph@spiced-academy.com>
2026-05-14 16:36:26 +0200

    Update the dashboard with totals and provide javascriipt functionality to retrieve this ids

--- core/analytics/static/analytics/js/dashboard.js
+++ core/analytics/static/analytics/js/dashboard.js
@@ -2,8 +2,16 @@ console.log("dashboard.js loaded");
 fetch("/api/kpis/")
     .then(response => response.json())
     .then(data => {
-        document.getElementById("kpi-output").textContent = JSON.stringify(data, null, 2);
+        document.getElementById("total-impressions").textContent = data.total_impressions
+        document.getElementById("total-clicks").textContent = data.total_clicks
+        document.getElementById("total-conversions").textContent = data.total_conversions
+        document.getElementById("total-cost").textContent = data.total_cost
+        document.getElementById("total-revenue").textContent = "€ " + data.total_revenue
     })
     .catch(error => {
-        document.getElementById("kpi-output").textContent = "Error loading KPI data: " + error;
+        document.getElementById("total-impressions").textContent = "Error loading data: ";
+        document.getElementById("total-clicks").textContent = "Error loading data: "
+        document.getElementById("total-conversions").textContent = "Error loading data";
+        document.getElementById("total-revenue").textContent = "Error loading data";
+        document.getElementById("roas").textContent = "Error loading data";
     });
