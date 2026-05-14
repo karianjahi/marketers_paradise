cd8f98a54b12026608521e5e13be1552b227641c
karianjahi <joseph@spiced-academy.com>
2026-05-14 17:17:53 +0200

    Fetch all KPIs and format the dashboard as the numbers are displayed

--- core/analytics/static/analytics/js/dashboard.js
+++ core/analytics/static/analytics/js/dashboard.js
@@ -2,16 +2,26 @@ console.log("dashboard.js loaded");
 fetch("/api/kpis/")
     .then(response => response.json())
     .then(data => {
-        document.getElementById("total-impressions").textContent = data.total_impressions
-        document.getElementById("total-clicks").textContent = data.total_clicks
-        document.getElementById("total-conversions").textContent = data.total_conversions
-        document.getElementById("total-cost").textContent = data.total_cost
-        document.getElementById("total-revenue").textContent = "€ " + data.total_revenue
+        document.getElementById("total-impressions").textContent = data.total_impressions;
+        document.getElementById("total-clicks").textContent = data.total_clicks;
+        document.getElementById("total-conversions").textContent = data.total_conversions;
+        document.getElementById("total-cost").textContent = "€ " + data.total_cost;
+        document.getElementById("total-revenue").textContent = "€ " + data.total_revenue;
+        document.getElementById("ctr").textContent = data.ctr;
+        document.getElementById("cpc").textContent = data.cpc;
+        document.getElementById("cpa").textContent = data.cpa;
+        document.getElementById("roas").textContent = data.roas;
+        document.getElementById("conversion-rate").textContent = data.conversion_rate;
     })
     .catch(error => {
         document.getElementById("total-impressions").textContent = "Error loading data: ";
         document.getElementById("total-clicks").textContent = "Error loading data: "
         document.getElementById("total-conversions").textContent = "Error loading data";
         document.getElementById("total-revenue").textContent = "Error loading data";
+        document.getElementById("total-cost").textContent = "Error loading data";
+        document.getElementById("ctr").textContent = "Error loading data";
+        document.getElementById("cpc").textContent = "Error loading data";
+        document.getElementById("cpa").textContent = "Error loading data";
         document.getElementById("roas").textContent = "Error loading data";
+        document.getElementById("conversion-rate").textContent = "Error loading data";
     });
