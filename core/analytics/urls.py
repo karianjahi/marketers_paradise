a0e7d1d340f6be634cc9475626e595b475d1e304
karianjahi <joseph@spiced-academy.com>
2026-05-14 12:59:38 +0200

    Refactor urls to have full paths for analytics url fully on analytics.url

--- core/analytics/urls.py
+++ core/analytics/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import CSVUploadAPIView, CampaignListAPIView, CampaignDetailAPIView, KPIAPIView
 
 urlpatterns = [
-    path("upload/", CSVUploadAPIView.as_view(), name="csv-upload"),
-    path("campaigns/", CampaignListAPIView.as_view(), name="campaign-list"),
-    path("campaigns/<int:pk>", CampaignDetailAPIView.as_view(), name="campaign-detail"),
-    path("kpis/", KPIAPIView.as_view(), name="kpi-summary"),
+    path("api/upload/", CSVUploadAPIView.as_view(), name="csv-upload"),
+    path("api/campaigns/", CampaignListAPIView.as_view(), name="campaign-list"),
+    path("api/campaigns/<int:pk>", CampaignDetailAPIView.as_view(), name="campaign-detail"),
+    path("api/kpis/", KPIAPIView.as_view(), name="kpi-summary"),
 ]
 
