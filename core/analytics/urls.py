a93e6bf48fdbdea9bb849a7025dde3c475d1290b
karianjahi <joseph@spiced-academy.com>
2026-05-15 15:02:00 +0200

    Refactor urls using pep8

--- core/analytics/urls.py
+++ core/analytics/urls.py
@@ -1,15 +1,30 @@
 from django.urls import path
-from .views import CSVUploadAPIView, CampaignListAPIView, CampaignDetailAPIView, KPIAPIView, dashboard, KPIByChannelAPIView, CampaignOptionsAPIView, CSVUploadLogListAPIView
+from .views import (
+    CSVUploadAPIView,
+    CampaignListAPIView,
+    CampaignDetailAPIView,
+    KPIAPIView,
+    dashboard,
+    KPIByChannelAPIView,
+    CampaignOptionsAPIView,
+    CSVUploadLogListAPIView,
+)
 
 urlpatterns = [
     path("api/upload/", CSVUploadAPIView.as_view(), name="csv-upload"),
     path("api/campaigns/", CampaignListAPIView.as_view(), name="campaign-list"),
-    path("api/campaigns/<int:pk>/", CampaignDetailAPIView.as_view(), name="campaign-detail"),
+    path(
+        "api/campaigns/<int:pk>/",
+        CampaignDetailAPIView.as_view(),
+        name="campaign-detail",
+    ),
     path("api/kpis/", KPIAPIView.as_view(), name="kpi-summary"),
     path("", dashboard, name="dashboard"),
     path("api/kpis/by-channel/", KPIByChannelAPIView.as_view(), name="kpis-by-channel"),
-    path("api/campaign-options/", CampaignOptionsAPIView.as_view(), name="campaign-options"),
+    path(
+        "api/campaign-options/",
+        CampaignOptionsAPIView.as_view(),
+        name="campaign-options",
+    ),
     path("api/upload-logs/", CSVUploadLogListAPIView.as_view(), name="upload-log-list"),
-    
 ]
-
